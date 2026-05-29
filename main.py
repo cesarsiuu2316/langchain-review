197408aa837e77325bf4205542df4e20f8d25678
cesar <cesar23augusto16@gmail.com>
2026-05-29 16:10:33 -0600

    added intro.py file and .env example file

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from langchain-review!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ updated_langchain/intro.py
@@ -0,0 +1,9 @@
+# Review langchain core concepts
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
