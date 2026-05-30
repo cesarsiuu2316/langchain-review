ed6f5f7f88a32dcf859fb462aea3864e000c9b6f
cesar <cesar23augusto16@gmail.com>
2026-05-29 23:35:47 -0600

    added llm initializations with init chat model and with specific integrations for groq

--- updated_langchain/intro.py
+++ updated_langchain/intro.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
+from langchain.chat_models import init_chat_model
+from langchain_groq import ChatGroq
 
 load_dotenv()
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
@@ -21,16 +23,31 @@ def make_agent(model):
     )
     return agent
 
+def create_llm_chat_model(model):
+    chat_model = init_chat_model(model)
+    return chat_model
+
+def create_groq_chat_model(model):
+    chat_model = ChatGroq(model=model)
+    return chat_model
+
 def main():
     agent = make_agent("groq:llama-3.3-70b-versatile")
+    chat_model = create_llm_chat_model("groq:llama-3.3-70b-versatile")
+    groq_chat_model = create_groq_chat_model("llama-3.3-70b-versatile")
+
     continue_chat = True
     while continue_chat:
         user_input = input("Ask a question (or type 'exit' to quit): ")
         if user_input.lower() == "exit":
             continue_chat = False
         else:
-            response = agent.invoke({"messages": [{"role": "user", "content": user_input}]})
-            print(f"AI user response: {response['messages'][-1]}")
+            response1 = agent.invoke({"messages": [{"role": "user", "content": user_input}]})
+            print(f"AI user response: {response1['messages'][-1]}\n")
+            response2 = chat_model.invoke(user_input)
+            print(f"LLM chat model response: {response2.content}\n")
+            response3 = groq_chat_model.invoke(user_input)
+            print(f"Groq chat model response: {response3.content}\n")
 
 if __name__ == "__main__":
     main()
